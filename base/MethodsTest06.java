a5f34e767e7506476a8231f002ced98fb6e539cf
yangweixin1406 <327621797@qq.com>
2020-06-01 19:44:20 +0800

    return 语句

---
+++ base/MethodsTest06.java
@@ -0,0 +1,10 @@
+public class MethodsTest06 {
+    public static void main (String[] args) {
+        int num = divide(8, 2);
+        System.out.println(num);
+    }
+
+    public static int divide (int a, int b) {
+        return a / b;
+    }
+}
