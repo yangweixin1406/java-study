6d1b6e2fef5a0575673670063d69047e64c90416
yangweixin1406 <327621797@qq.com>
2020-06-01 17:49:10 +0800

    类型转换

---
+++ base/MethodsTest04.java
@@ -0,0 +1,9 @@
+public class MethodsTest04 {
+    public static void main (String[] args) {
+        MethodsTest04.sumInt(4, 89);
+    }
+    public static void sumInt (long a, long b) {
+        System.out.println(a + " + " + b + " = " + (a + b) );
+    }
+}
+
